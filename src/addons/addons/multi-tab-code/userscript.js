4309541c51215278dd0453f7462e094eac17e93d
redman13 <godslayerakp@gmail.com>
2025-07-02 10:51:08 -0700

    dont add if the block is already there, actually access the value

--- src/addons/addons/multi-tab-code/userscript.js
+++ src/addons/addons/multi-tab-code/userscript.js
@@ -92,13 +92,13 @@ export default async function ({ addon, msg, console }) {
     if (e.type === 'delete' && !scriptsHasBlock(tabs[selectedTab].scripts, e.blockId)) return;
     if (e.type === 'create' && !tabs[selectedTab].scripts.includes(e.blockId))
       tabs[selectedTab].scripts.push(e.blockId);
-    if (e.type === 'move' && !e.newParent)
+    if (e.type === 'move' && !e.newParentId && !tabs[selectedTab].scripts.includes(e.blockId))
       tabs[selectedTab].scripts.push(e.blockId);
-    if (e.type === 'move' && e.newParent) {
+    if (e.type === 'move' && e.newParentId && tabs[selectedTab].scripts.includes(e.blockId)) {
       const idx = tabs[selectedTab].scripts.indexOf(e.blockId);
       tabs[selectedTab].scripts.splice(idx, 1);
     }
-    if (e.type === 'delete') {
+    if (e.type === 'delete' && tabs[selectedTab].scripts.includes(e.blockId)) {
       const idx = tabs[selectedTab].scripts.indexOf(e.blockId);
       tabs[selectedTab].scripts.splice(idx, 1);
     }
